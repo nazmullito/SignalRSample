38c39e061b9594e294a4505a0e4c6e7e729495c3
nazmul <NazmulIslam@NAZMUL-PC>
2023-05-15 02:29:05 +0600

    Notification

--- SignalRSample/Program.cs
+++ SignalRSample/Program.cs
@@ -44,5 +44,5 @@ app.MapRazorPages();
 app.MapHub<UserHub>("/hubs/userCount");
 app.MapHub<DeathlyHallowsHub>("/hubs/deathlyhallows");
 app.MapHub<HouseGroupHub>("/hubs/houseGroup");
- 
-app.Run(); 
+app.MapHub<NotificationHub>("/hubs/notification");
+app.Run();

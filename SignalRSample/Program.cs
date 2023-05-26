c0b2df76704e1a23c755eff6cc45370abf3ad2bb
nazmul <NazmulIslam@NAZMUL-PC>
2023-05-27 05:45:21 +0600

    hub connection helper

--- SignalRSample/Program.cs
+++ SignalRSample/Program.cs
@@ -45,6 +45,7 @@ app.MapHub<UserHub>("/hubs/userCount");
 app.MapHub<DeathlyHallowsHub>("/hubs/deathlyhallows");
 app.MapHub<HouseGroupHub>("/hubs/houseGroup");
 app.MapHub<NotificationHub>("/hubs/notification");
-app.MapHub<ChatHub>("/hubs/basicchat");
+app.MapHub<BasicChatHub>("/hubs/basicchat");
+app.MapHub<ChatHub>("/hubs/chat");
 app.MapHub<OrderHub>("/hubs/order");
 app.Run();

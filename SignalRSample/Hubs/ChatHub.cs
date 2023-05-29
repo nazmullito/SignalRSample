47792ac0bde500fbfed595c8874ad0986d85d56c
nazmul <NazmulIslam@NAZMUL-PC>
2023-05-30 02:59:36 +0600

    created room

--- SignalRSample/Hubs/ChatHub.cs
+++ SignalRSample/Hubs/ChatHub.cs
@@ -19,11 +19,51 @@ namespace SignalRSample.Hubs
             if (!string.IsNullOrEmpty(UserId) )
             {
                 var userName = _db.Users.FirstOrDefault(u => u.Id == UserId).UserName;
-                Clients.Users(HubConnections.OnlineUsers()).SendAsync("ReceiveUserConnected", UserId, userName, HubConnections.HasUser(UserId));
+                Clients.Users(HubConnections.OnlineUsers()).SendAsync("ReceiveUserConnected", UserId, userName);
                 HubConnections.AddUserConnection(UserId, Context.ConnectionId);
             }
             return base.OnConnectedAsync();
         }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            var UserId = Context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (HubConnections.HasUserConnection(UserId, Context.ConnectionId))
+            {
+                var UserConnections = HubConnections.Users[UserId];
+                UserConnections.Remove(Context.ConnectionId);
+
+                HubConnections.Users.Remove(UserId);
+                if (UserConnections.Any())
+                    HubConnections.Users.Add(UserId, UserConnections);
+            }
+
+            if (!String.IsNullOrEmpty(UserId))
+            {
+                var userName = _db.Users.FirstOrDefault(u => u.Id == UserId).UserName;
+                Clients.Users(HubConnections.OnlineUsers()).SendAsync("ReceiveUserDisconnected", UserId, userName);
+                HubConnections.AddUserConnection(UserId, Context.ConnectionId);
+            }
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendAddRoomMessage(int maxRoom, int roomId, string roomName)
+        {
+            var UserId = Context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var userName = _db.Users.FirstOrDefault(u => u.Id == UserId).UserName;
+
+            await Clients.All.SendAsync("ReceiveAddRoomMessage", maxRoom, roomId, roomName, UserId, userName);
+        }
+
+        public async Task SendDeleteRoomMessage(int deleted, int selected, string roomName)
+        {
+            var UserId = Context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var userName = _db.Users.FirstOrDefault(u => u.Id == UserId).UserName;
+
+            await Clients.All.SendAsync("ReceiveDeleteRoomMessage", deleted, selected, roomName, userName);
+        }
+
         //public async Task SendMessageToAll(string user, string message)
         //{
         //    await Clients.All.SendAsync("MessageReceived", user, message);
--- SignalRSample/wwwroot/js/chat.js
+++ SignalRSample/wwwroot/js/chat.js
@@ -3,11 +3,116 @@
     .withAutomaticReconnect([0, 1000, 5000, null])
     .build();
 
-connection.on("ReceiveUserConnected", function (userId, userName, isOldConnection) {
-    if (!isOldConnection) {
-        addMessage(`${userName} is online`);
-    }
+connection.on("ReceiveUserConnected", function (userId, userName) {
+        addMessage(`${userName} has openned a connection`);
 });
+
+connection.on("ReceiveUserDisconnected", function (userId, userName) {
+        addMessage(`${userName} has closed a connection`);
+}); 
+
+connection.on("ReceiveAddRoomMessage", function (maxRoom, roomId, roomName, userId, userName) {
+    addMessage(`${userName} has created room  ${roomName}`);
+    fillRoomDropDown();
+});
+
+
+function addnewRoom(maxRoom) {
+    let createRoomName = document.getElementById('createRoomName');
+    var roomName = createRoomName.value;
+    if (roomName == null && roomName == '') {
+        return;
+    }
+
+    /*POST*/
+    $.ajax({
+        url: '/ChatRooms/PostChatRoom',
+        dataType: "json",
+        type: "POST",
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify({ id: 0, name: roomName }),
+        async: true,
+        processData: false,
+        cache: false,
+        success: function (json) {
+            /*ADD ROOM COMPLETED SUCCESSFULLY*/
+            connection.send("SendAddRoomMessage", maxRoom, json.id, json.name);
+            createRoomName.value = '';
+        },
+        error: function (xhr) {
+            alert('error');
+        }
+    })
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    fillRoomDropDown();
+    fillUserDropDown();
+})
+
+function fillUserDropDown() {
+
+    $.getJSON('/ChatRooms/GetChatUser')
+        .done(function (json) {
+
+            var ddlSelUser = document.getElementById("ddlSelUser");
+
+            ddlSelUser.innerText = null;
+
+            json.forEach(function (item) {
+                var newOption = document.createElement("option");
+
+                newOption.text = item.userName;//item.whateverProperty
+                newOption.value = item.id;
+                ddlSelUser.add(newOption);
+
+
+            });
+
+        })
+        .fail(function (jqxhr, textStatus, error) {
+
+            var err = textStatus + ", " + error;
+            console.log("Request Failed: " + jqxhr.detail);
+        });
+
+}
+
+function fillRoomDropDown() {
+
+    $.getJSON('/ChatRooms/GetChatRoom')
+        .done(function (json) {
+            var ddlDelRoom = document.getElementById("ddlDelRoom");
+            var ddlSelRoom = document.getElementById("ddlSelRoom");
+
+            ddlDelRoom.innerText = null;
+            ddlSelRoom.innerText = null;
+
+            json.forEach(function (item) {
+                var newOption = document.createElement("option");
+
+                newOption.text = item.name;
+                newOption.value = item.id;
+                ddlDelRoom.add(newOption);
+
+
+                var newOption1 = document.createElement("option");
+
+                newOption1.text = item.name;
+                newOption1.value = item.id;
+                ddlSelRoom.add(newOption1);
+
+            });
+
+        })
+        .fail(function (jqxhr, textStatus, error) {
+
+            var err = textStatus + ", " + error;
+            console.log("Request Failed: " + jqxhr.detail);
+        });
+
+}
+
 
 function addMessage(msg) {
     if (msg == null && msg == '') {
@@ -15,7 +120,7 @@ function addMessage(msg) {
     }
     let ui = document.getElementById('messagesList');
     let li = document.createElement("li");
-    li.innerHTML = msg;
+    li.innerHTML = msg; 
     ui.appendChild(li);
 }
 
